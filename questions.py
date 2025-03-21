5e1ec658fc96dfed25d1b7f5648dec631067dea0
NICO <Nisholas15@gmail.com>
2025-03-20 23:02:38 -0300

    calcula  puntaje

--- questions.py
+++ questions.py
@@ -21,7 +21,7 @@ answers = [
      ]
  # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
-
+score = 0
  # El usuario deberá contestar 3 preguntas
 for _ in range(3):
      # Se selecciona una pregunta aleatoria
@@ -37,16 +37,20 @@ for _ in range(3):
          if(user_answer in ["1","2","3","4"]):
             user_answer = int(user_answer)-1 
          else:
+             print("ingrese un nro entre 1 y 4")
              exit(1)      
      # Se verifica si la respuesta es correcta
          if user_answer == correct_answers_index[question_index]:
              print("¡Correcto!")
+             score+=1
              break
      else:
      # Si el usuario no responde correctamente después de 2 intentos,
      # se muestra la respuesta correcta
          print("Incorrecto. La respuesta correcta es:")
          print(answers[question_index] [correct_answers_index[question_index]])
+         score-=0.5
      # Se imprime un blanco al final de la pregunta
-#se hizo ajustes en otras lineas por errores de sangria
-     print("")
+#tu puntaje final es:
+print(f"Tu puntaje es: {score}")
+print("")
