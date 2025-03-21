ddb4f144754595a11ffe3247470ed08dcc039ec2
NICO <Nisholas15@gmail.com>
2025-03-20 22:51:27 -0300

    solucion bug

--- questions.py
+++ questions.py
@@ -1,13 +1,13 @@
 import random
  # Preguntas para el juego
- questions = [
+questions = [
      "¿Qué función se usa para obtener la longitud de una cadena en Python?",
      "¿Cuál de las siguientes opciones es un número entero en Python?",
      "¿Cómo se solicita entrada del usuario en Python?",
      "¿Cuál de las siguientes expresiones es un comentario válido en Python?",
      "¿Cuál es el operador de comparación para verificar si dos valores son iguales?",]
  # Respuestas posibles para cada pregunta, en el mismo orden que las preguntas
- answers = [
+answers = [
      ("size()", "len()", "length()", "count()"),
      ("3.14", "'42'", "10", "True"),
      ("input()", "scan()", "read()", "ask()"),
@@ -20,9 +20,10 @@ import random
      ("=", "==", "!=", "==="),
      ]
  # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
- correct_answers_index = [1, 2, 0, 3, 1]
+correct_answers_index = [1, 2, 0, 3, 1]
+
  # El usuario deberá contestar 3 preguntas
- for _ in range(3):
+for _ in range(3):
      # Se selecciona una pregunta aleatoria
      question_index = random.randint(0, len(questions) - 1)
      # Se muestra la pregunta y las respuestas posibles
@@ -31,7 +32,12 @@ import random
          print(f"{i + 1}. {answer}")
      # El usuario tiene 2 intentos para responder correctamente
      for intento in range(2):
-         user_answer = int(input("Respuesta: ")) - 1
+         user_answer = input("RESPUESTA: ")
+         #asegurar nro ingresado
+         if(user_answer in ["1","2","3","4"]):
+            user_answer = int(user_answer)-1 
+         else:
+             exit(1)      
      # Se verifica si la respuesta es correcta
          if user_answer == correct_answers_index[question_index]:
              print("¡Correcto!")
@@ -42,4 +48,5 @@ import random
          print("Incorrecto. La respuesta correcta es:")
          print(answers[question_index] [correct_answers_index[question_index]])
      # Se imprime un blanco al final de la pregunta
-     print()
+#se hizo ajustes en otras lineas por errores de sangria
+     print("")
